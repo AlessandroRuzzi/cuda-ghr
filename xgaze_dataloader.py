732141c6c71bc45ea2739648a8d37664fc1366b3
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-10-26 22:18:01 +0200

    u

--- xgaze_dataloader.py
+++ xgaze_dataloader.py
@@ -16,7 +16,7 @@ trans_train = transforms.Compose([
         #transforms.ToTensor(),  # this also convert pixel value from [0,255] to [0,1]
         #transforms.Normalize(mean=[0.485, 0.456, 0.406],
         #                     std=[0.229, 0.224, 0.225]),
-        transforms.Resize(size=(128,128)),
+        transforms.Resize(size=(256,256)),
     ])
 
 trans = transforms.Compose([
@@ -24,7 +24,7 @@ trans = transforms.Compose([
         #transforms.ToTensor(),  # this also convert pixel value from [0,255] to [0,1]
         #transforms.Normalize(mean=[0.485, 0.456, 0.406],
         #                     std=[0.229, 0.224, 0.225]),
-        transforms.Resize(size=(128,128)),
+        transforms.Resize(size=(256,256)),
     ])
 
 
