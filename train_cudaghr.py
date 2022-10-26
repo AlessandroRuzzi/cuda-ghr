d8c3df74cbaf1943c4c33378b0ca5221702b89a1
AlessandroRuzzi <alrxruzzi98@gmail.com>
2022-10-26 20:13:28 +0200

    fix device

--- train_cudaghr.py
+++ train_cudaghr.py
@@ -60,8 +60,8 @@ trans_resize = transforms.Compose(
     ]
 )
 
-mean = torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1)
-std = torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1)
+mean = torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1).to(device)
+std = torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1).to(device)
         
 
 torch.manual_seed(45)  # cpu
